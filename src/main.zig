c80bf349a040bb0b6bde400fcc659e52d7081a5a
LeakyBucket <glen@leakybucket.dev>
2025-08-01 14:20:34 -0600

    Initial logic to fetch over HTTP (RSS)

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,48 @@
+var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
 
 pub fn main() !void {
+    // Dig deeper into what exactly this is doing.  I'm confident that the double "incline" const declarations effectively decompose what the
+    // switch expression is returning.  However I'm not sure about the `gpa:` (tagged scope?) bit wrapping the switch and why there is a break
+    // there.
+    const allocator, const is_debug = gpa: {
+        break :gpa switch (builtin.mode) {
+            .Debug, .ReleaseSafe => .{ debug_allocator.allocator(), true },
+            .ReleaseFast, .ReleaseSmall => .{ std.heap.smp_allocator, false },
+        };
+    };
+    defer if (is_debug) {
+        _ = debug_allocator.deinit();
+    };
+
+    // Create a new http Client
+    var http_client = std.http.Client{ .allocator = allocator };
+    defer http_client.deinit();
+
+    // Create an ArrayList to hold the response data.  An ArrayList is a dynamically growable data structure.
+    var http_body = std.ArrayList(u8).init(allocator);
+    defer http_body.deinit();
+
+    // The URL we want to visit for this test.
+    const url = "https://pluralistic.net/feed/";
+
+    _ = try fetch_rss(url, &http_client, &http_body);
+
+    const response_slice = try http_body.toOwnedSlice();
+
+    std.debug.print("{s}", .{response_slice});
+}
+
+// For some reason the compiler is not happy with the `uri_string` declaration in the head here.  I thought a `[]u8` would work for a string
+// of any length but it complains that I am receiving an array with very explicit dimensions (including null termination).
+//
+// Turns out I was missing the `const` nature of the pointer.  Since it is a string compiled into the binary here the address can't change...
+fn fetch_rss(uri_string: []const u8, client: *std.http.Client, response_list: *std.ArrayList(u8)) !std.http.Status {
+    const uri = try std.Uri.parse(uri_string);
+
+    const response = try client.fetch(.{ .method = .GET, .location = .{ .uri = uri }, .response_storage = .{ .dynamic = response_list }, .headers = .{ .accept_encoding = .{ .override = "application/rss" } } });
+
+    return response.status;
 }
 
 const std = @import("std");
+const builtin = @import("builtin");
